70427109edc69c5e0905adb69d551bfbbff3537c
Shuichiro Kamiya <notsunohito@gmail.com>
2015-03-03 23:30:31 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('mongod', function () {
     });
 });
 
-gulp.task('seeds', function () {
+gulp.task('seed', function () {
     exec('mongoimport --db doyadb -collection users --file config/mongodb/data/seeds/users.json', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
