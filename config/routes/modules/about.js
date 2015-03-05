5ba8ec130274b5a6d9828acfe78ed0c3e4f6edb1
Shuichiro Kamiya <notsunohito@gmail.com>
2015-03-05 17:40:34 +0900

    Implement layout_session.ejs

--- config/routes/modules/about.js
+++ config/routes/modules/about.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 var about = require('../../../app/controllers/about');
 
-router.get('/', function(req, res, next) {
-    res.render('about/index', about.index());
-});
-
+router.get('/', about.index);
 
 module.exports = router;
--- config/routes/modules/coffeehouses.js
+++ config/routes/modules/coffeehouses.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = express.Router();
 var coffeehouse = require('../../../app/controllers/coffeehouse');
 
-router.get('/', function(req, res, next) {
-    res.render('coffeehouse/index', coffeehouse.index());
-});
+router.get('/', coffeehouse.index);
 
 module.exports = router;
