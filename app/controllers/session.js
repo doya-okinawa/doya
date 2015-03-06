aee786918b7606f973b5bf643f6e0a6d019f23c2
Shuichiro Kamiya <notsunohito@gmail.com>
2015-03-06 18:59:06 +0900

    Refactoring routes.js

--- app/controllers/session.js
+++ app/controllers/session.js
@@ -1,24 +1,27 @@
+var passport = require('passport');
+
 var SessionController = {
     login: function(req, res, next) {
         return res.render('session/login', {});
     },
-    new: function(req, res, next) {
-        req.flash('notice', 'ログインしました');
-        return res.redirect('/');
-    },
-    membersonly: function(req, res, next) {
-        if(!req.isAuthenticated()) return next();
-        return res.render('session/membersonly', {});
-    },
+    create: [ passport.authenticate('local', { failureRedirect: '/login', failureFlash: { type: 'notice', message: 'ログインに失敗しました' }}),
+              function(req, res, next) {
+                  req.flash('notice', 'ログインしました');
+                  return res.redirect('/');
+              }],
+    membersonly: [ function (req, res, next) {
+                       if(req.isAuthenticated()) return next();
+                       req.flash('notice', 'ログインしてください');
+                       return res.redirect("/login");
+                   },
+                   function(req, res, next) {
+                       if(!req.isAuthenticated()) return next();
+                       return res.render('session/membersonly', {});
+                   }],
     logout: function(req, res, next) {
         req.logout();
         req.flash('notice', 'ログアウトしました');
         return res.redirect("/");
-    },
-    isLogined: function(req, res, next) {
-        if(req.isAuthenticated()) return next();
-        req.flash('notice', 'ログインしてください');
-        return res.redirect("/login");
     }
 };
 
